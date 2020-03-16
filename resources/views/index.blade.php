b709a2d1101ce242524af0e80f8035b8cae2ca39
Lucas Dauto <lucasdauto79@gmail.com>
2020-03-16 12:03:59 -0300

    Criação home

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('template.index')
+
+@section('titulo','Inicio')
+
+@section('conteudo')
+    <div class="row justify-content-center">
+        <div class="col-4 pt-5">
+            <h1>Bem-vindo ao Sisdenys</h1>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('template.index');
-});
+    return view('index');
+})->name('home');
